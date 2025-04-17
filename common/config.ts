be12253e6922a44d85b4b0f2d2575d4a84b25cba
nextuser <nextuser@163.com>
2025-04-18 07:33:18 +0800

    change config of faucet limit

--- common/config.ts
+++ common/config.ts
@@ -1,6 +1,6 @@
 export const faucet_config={
-    mainnet_balance_limit : 1e8,
-    faucet_amount : 1e9,
+    mainnet_balance_limit : 1e9,
+    faucet_amount : 1e8,
     gas_budget: 3e8,
     faucet_address : '0x16781b5507cafe0150fe3265357cccd96ff0e9e22e8ef9373edd5e3b4a808884',
     rpc_url: '/v1/gas'
